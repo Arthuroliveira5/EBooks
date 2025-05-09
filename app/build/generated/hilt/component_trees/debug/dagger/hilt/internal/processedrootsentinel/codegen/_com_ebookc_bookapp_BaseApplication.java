f8ba86635291b76a0930f4a9162a6f57405c0998
Arthur Luiz <devarthur90@gmail.com>
2025-05-09 11:30:44 -0300

    Atualizado .gitignore para ignorar arquivos gerados

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_ebookc_bookapp_BaseApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.ebookc.bookapp.BaseApplication"
-)
-public final class _com_ebookc_bookapp_BaseApplication {
-}
